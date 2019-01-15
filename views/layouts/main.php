dc1a61356ebe5601d2b5267b8127ef5d81410e21
kva <vasickru@mail.ru>
2019-01-15 13:44:16 +0500

    Initial commit

---
+++ views/layouts/main.php
@@ -0,0 +1,57 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<section class="body">
+    <header>
+        <div class="container">
+            <div class="header">
+                <a href="index.html">На главную</a>
+                <a href="#">Вход в админку</a>
+                <a href="#">Корзина</a>
+                <input type="text" style="padding: 5px" placeholder="Поиск..." name="search">
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <?= $content ?>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="footer">
+                &copy; Все права защищены или типа того
+            </div>
+        </div>
+    </footer>
+</section>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
